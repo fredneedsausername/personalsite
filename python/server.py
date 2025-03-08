bb6f7a1820cef529be4bc3b7c9813469bc597292
fred ver <fred.ver06@gmail.com>
2025-03-09 00:57:18 +0100

    Added lezioni with leandro page

--- python/server.py
+++ python/server.py
@@ -303,6 +303,10 @@ if __name__ == "__main__":
     @app.route("/acca")
     def acca():
         return render_template("acca.html")
+    
+    @app.route("/lezioni-c-con-leandro")
+    def lezioni_c_con_leandro():
+        return render_template("lezioni-c-con-leandro.html")
 
     # serve(app, host="127.0.0.1", port=42069)
     app.run(host = "127.0.0.1", port = 5000, debug = True)
